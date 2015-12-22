901d6c741615ad9d0b861dd5ea99dda3faf60233
Emberald <simonzhangintp@gmail.com>
2015-12-22 05:28:59 -0800

    Implement with asynchronous I/O

--- dslib/fetcher.py
+++ dslib/fetcher.py
@@ -1,75 +1,77 @@
-import time
-
 import utils
 logger = utils.get_logger(__name__)
 
-from requests import Session
-s = Session()
+import asyncio
+from aiohttp import ClientSession, Timeout
 
-headers = {
+
+_HOSTS = (
+    'comment.bilibili.com',
+    'comment.bilibili.tv'
+)
+_DEFAULT_HEADER = {
 	# 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/600.7.12 (KHTML, like Gecko) Version/8.0.7 Safari/600.7.12',
 	'Referer': 'www.baidu.com'
 }
-s.headers = headers
+_host = _HOSTS[0]
+_header = _DEFAULT_HEADER
+_timeout = 5 # connect timeout
+_cooldown_duration = 1 # TODO adjust according to polite time
 
-connect_timeout = 5
-read_timeout = 10
-cooldown_duration = 1 # TODO adjust according to polite time
 
-host = 'comment.bilibili.com'
-alt_hosts = set([
-    'comment.bilibili.com',
-    'comment.bilibili.tv'
-])
+async def get(cid, timestamp=0, *, timeout=5, headers=None):
+	_timeout = timeout
+	if headers is not None:
+		_ensure_connection(headers)
 
-def get_xml(cid):
-	return _get_xml(cid, 0)
-
-def _get_xml(cid, timestamp):
-	pass
+	return await _get_xml(cid, timestamp)
 
 # TODO ToTest
 def config(**kwargs):
-	# TODO type of exporter
 	auto = kwargs.pop('autohost', None)
 	if auto:
 		_connect_faster_host()
-	cto = kwargs.pop('connect_timeout', None)
-	if cto:
-		connect_timeout = cto
-	rto = kwargs.pop('read_timeout', None)
-	if rto:
-		read_timeout = rto
-	headers.update(kwargs) # TODO strip invalid key-values
 
-def _get_url_contents(url):
+async def _get_xml(cid, timestamp=0):
+	if timestamp is 0:
+		url = '%s/%d.xml' % (host, cid)
+	else
+		url = '%s/dmroll,%d,%d' % (host, timestamp, cid)
+
+	$raw = await _get_url_contents(url)
+	# TODO
+
+async def _get_url_contents(url):
 	if not url:
-		return None
-	tries = 0
-	while True:
+		raise RuntimeError('No url given')
+	if not _ensure_connection() and _count_requests is not 0:
+		logger.debug('Session was closed unexpectedly')
+	_count_requests += 1
+
+	logger.debug('Start getting url content')
+	for tries in range(1, _RETRY + 1):
 		try:
-			logger.debug('Start getting url content')
-			resp = s.get(url, timeout=(connect_timeout, read_timeout))
+			with Timeout(timeout):
+				async with _session.get(url) as resp:
+					raw = await resp.read()
 		except Exception as e:
-			tries += 1
-			logger.info('Failed to get url content for the %s time', _ORDINAL[tries])
-			if tries < _RETRY:
-				time.sleep(cooldown_duration)
-				logger.debug('Retrying')
-			else:
-				logger.warning('Failed to request content of %s for %s', url, e)
-				return None
+			logger.info('Failed to get url content for the %s time for %s', _ORDINAL[tries], e)
 		else:
-			content = resp.content # TODO encoding
-			if not content:
-				# TODO raise, log
-				pass
-			logger.debug('Finish getting url content: %s', content)
-			return content
+			logger.debug('Finish getting url content: %s', raw)
+			return raw
+	logger.warning('Failed to request content of %s', url)
+	raise
 
-_RETRY = 3
 _ORDINAL = (None, '1st', '2nd', '3rd')
+_RETRY = 3
+_session = None
+_count_requests = 0
 
+def _ensure_connection(headers=_header):
+	if not _session or _session.closed:
+		_session = ClientSession(headers=headers)
+		return False
+	return True
 
 # TODO
 def _connect_faster_host():
