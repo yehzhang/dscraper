28ce7d8a06d0032ba188907b9841de6fca03b651
yehzhang <simonzhangintp@gmail.com>
2016-01-30 17:49:44 -0800

    Escape invalid XML characters before parsing

--- dscraper/utils.py
+++ dscraper/utils.py
@@ -63,8 +63,10 @@ def get_headers_text(headers):
 
 def get_status_code(raw):
     match = _PATTERN_ST.search(raw)
-    if match:
+    try:
         return int(match.group(1))
+    except TypeError:
+        pass
 
 _PATTERN_ST = re.compile(b'HTTP/1.1 (\d+) ')
 
@@ -78,17 +80,36 @@ def inflate_and_decode(raw):
         _logger.debug('cannot decode: \n%s', raw)
         raise DecodeError('failed to decode the data from the response') from e
 
-def parse_xml(text):
-    # TODO XML string with invalid characters
+def parse_comments_xml(text):
+    # Escape invalid chracters with their hexadecimal notations
+    text = _PATTERN_ILL_XML_CHR.sub(_REPL_ILL_XML_CHR, text)
     try:
         root = et.fromstring(text)
-    except et.ParseError as e: # TODO what exception means what?
+    except et.ParseError as e:
         raise ParseError('failed to parse the XML data') from e
     if root.text == 'error':
         raise ContentError('the XML data contains a single element with "error" as content')
+    # TODO split attrs
     return root
 
-def parse_json(text):
+illegal_xml_chrs = [(0x00, 0x08), (0x0B, 0x0C),
+                     (0x0E, 0x1F), (0x7F, 0x84),
+                     (0x86, 0x9F), (0xFDD0, 0xFDDF),
+                     (0xFFFE, 0xFFFF), (0x1FFFE, 0x1FFFF),
+                     (0x2FFFE, 0x2FFFF), (0x3FFFE, 0x3FFFF),
+                     (0x4FFFE, 0x4FFFF), (0x5FFFE, 0x5FFFF),
+                     (0x6FFFE, 0x6FFFF), (0x7FFFE, 0x7FFFF),
+                     (0x8FFFE, 0x8FFFF), (0x9FFFE, 0x9FFFF),
+                     (0xAFFFE, 0xAFFFF), (0xBFFFE, 0xBFFFF),
+                     (0xCFFFE, 0xCFFFF), (0xDFFFE, 0xDFFFF),
+                     (0xEFFFE, 0xEFFFF), (0xFFFFE, 0xFFFFF),
+                     (0x10FFFE, 0x10FFFF)]
+illegal_ranges = [r'{}-{}'.format(chr(low), chr(high)) for low, high in illegal_xml_chrs]
+_PATTERN_ILL_XML_CHR = re.compile(r'[{}]'.format(r''.join(illegal_ranges)))
+_REPL_ILL_XML_CHR = lambda x: r'\x{:02X}'.format(ord(x.group(0)))
+del illegal_xml_chrs, illegal_ranges
+
+def parse_rolldate_json(text):
     try:
         return json.loads(text)
     except json.JSONDecodeError as e:
@@ -96,6 +117,9 @@ def parse_json(text):
 
 def merge_xmls(xmls):
     # TODO
+    pass
+
+def strip_invalid_xml_chars(text):
     pass
 
 def capitalize(s):
