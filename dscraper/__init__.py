aafc5c536096e83a5db352300f0caa7f5970d406
yehzhang <simonzhangintp@gmail.com>
2016-01-31 20:17:11 -0800

    Add license

--- dscraper/__init__.py
+++ dscraper/__init__.py
@@ -1,11 +1,25 @@
-from . import utils
-from .fetcher import *
+"""
+dscraper
+~~~~~~~~
+# TODO
 
-__all__ = (fetcher.__all__)
+:copyright: (c) 2016 by Simon Zhang.
+:license: Apache 2.0, see LICENSE for more details.
 
-# print(__all__)
+"""
 
+__title__ = 'dscraper'
+__version__ = '0.2.0'
+__author__ = 'Simon Zhang'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Simon Zhang'
+
+import sys
+assert sys.version_info >= (3, 5)
+
+from .exceptions import HostError, DecodeError, PageNotFound
+from .scraper import Scraper
+from .exporter import StdoutExporter, FileExporter, MysqlExporter, SqliteExporter
 
 import logging
-
 logging.getLogger(__name__).addHandler(logging.NullHandler())
